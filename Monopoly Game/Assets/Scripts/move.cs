8c3ba6dba79e386618104fa116b8778227f7d329
DenusStasiuk <denuk2828@gmail.com>
2019-11-19 19:04:54 +0200

    add first script

---
+++ Monopoly Game/Assets/Scripts/move.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Gamer gamer1;
+    
+    List<Vector3> fields = new List<Vector3>();
+    
+    void Start()
+    {
+        fields.Add(new Vector3(-0.04f, -3.93f));
+        fields.Add(new Vector3(-1.16f, -3.93f));
+        fields.Add(new Vector3(-2.64f, -3.93f));
+        fields.Add(new Vector3(-4.06f, -3.93f));
+        fields.Add(new Vector3(-5.33f, -3.93f));
+        fields.Add(new Vector3(-6.5f, -3.93f));
+    }
+    void Update()
+    {
+
+        Vector3 v = fields[Random.Range(0,5)];
+        transform.position = v;
+        
+    }
+}
